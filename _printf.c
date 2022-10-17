b7f37c804e298ad50a702bff997565f9fb4bd2a2
Wendybella <favekoka@gmail.com>
2022-10-17 15:29:15 -0700

    tasks 0

--- _printf.c
+++ _printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
+ * _printf - Receives the main string and 
+ * all the necessary parameters to
  * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * @format: string containing all the desired characters
+ * Return: total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
